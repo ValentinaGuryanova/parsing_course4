177638c9b7b05a701e49fb33210f0470a84f724f
ValentinaGuryanova <gvalushka@mail.ru>
2023-08-20 21:02:59 +0300

    Проект курсовой 4 (в процессе доработки)

--- classes.py
+++ classes.py
@@ -1,25 +1,22 @@
 from abc import abstractmethod, ABC
 from exceptions import ParsingError
+from utils import get_currencies
 
 import requests
 import json
 import os
 
-#value = os.getenv('API_KEY')
+value = os.getenv('X-Api-App-Id')
 
 
 class Engine(ABC):
     """Абстрактный родительский класс"""
     @abstractmethod
     def get_request(self):
-        """Получаем информацию с сайта"""
-
         pass
 
     @abstractmethod
     def get_vacancies(self):
-        """Получаем вакансии"""
-
         pass
 
 class HeadHunterAPI(Engine):
@@ -28,24 +25,25 @@ class HeadHunterAPI(Engine):
     url = "https://api.hh.ru/vacancies/"
 
     def __init__(self, keyword):
-        self.vacancies = []
+
         self.params = {
             "per_page": 100,
             "page": None,
             "text": keyword,
-            "archive": False,
+            "archived": False,
         }
+        self.vacancies = []
 
     def get_request(self):
         response = requests.get(self.url, params=self.params)
         if response.status_code != 200:
             raise ParsingError(f"Ошибка получения вакансий! Статус: {response.status_code}")
-        return response.json() #["objects"]
+        return response.json()
 
     def get_formatted_vacancies(self):
         formatted_vacancies = []
-        #currencies = get_currencies()
-        sj_currencies = {
+        currency = get_currencies()
+        hh_currencies = {
             "rub": "RUR",
             "uah": "UAH",
             "uzs": "UZS",
@@ -53,16 +51,18 @@ class HeadHunterAPI(Engine):
 
         for vacancy in self.vacancies:
             formatted_vacancy = {
-                "employer": vacancy["firm_name"],
-                "title": vacancy["profession"],
-                "url": vacancy["link"],
-                "api": "SuperJob",
-                "salary_from": vacancy["payment_from"] if vacancy["payment_from"] and vacancy["payment_from"] != 0 else None,
-                "salary_to": vacancy["salary_to"] if vacancy["salary_to"] and vacancy["salary_to"] != 0 else None,
+                "employer": vacancy["id"],
+                "title": vacancy["name"],
+                "url": vacancy["url"],
+                "api": "HeadHunter",
+                "salary_from": vacancy["salary"]["from"] if vacancy["salary"] else None,
+                "salary_to": vacancy["salary"]["to"] if vacancy["salary"] else None,
+                # "currency": vacancy["currency"],
+                # "currency_value": vacancy["currency_value"]
             }
-            if vacancy["currency"] in sj_currencies:
-                formatted_vacancy["currency"] = sj_currencies[vacancy["currency"]]
-                formatted_vacancy["currency_value"] = sj_currencies[sj_currencies[vacancy["currency"]]] in sj_currencies[vacancy["currency"]]
+            if vacancy["currency"] in hh_currencies:
+                formatted_vacancy["currency"] = hh_currencies[vacancy["currency"]]
+                formatted_vacancy["currency_value"] = hh_currencies[hh_currencies[vacancy["currency"]]] in hh_currencies[vacancy["currency"]]
             elif vacancy["currency"]:
                 formatted_vacancy["currency"] = "RUR"
                 formatted_vacancy["currency_value"] = 1
@@ -75,29 +75,29 @@ class HeadHunterAPI(Engine):
         return formatted_vacancies
 
 
-    def get_vacancies(self, pages_count: int=2):
-        self.__class__.name__ = None
+    def get_vacancies(self, pages_count=2):
+        self.vacancies = []
         for page in range(pages_count):
             page_vacancies = []
             self.params["page"] = page
-            print(f"({self.__class__.name__}) Парсинг страницы { page } -", end=" ")
+            print(f"({self.__class__.__name__}) Парсинг страницы { page } -", end=" ")
             try:
                 page_vacancies = self.get_request()
             except ParsingError as error:
                 print(error)
             else:
-                self.vacancies.extend(page_vacancies)
-                print(f"Загружено вакансий: { len(page_vacancies) }")
-            if len(page_vacancies) == 0:
+                self.vacancies.extend(page_vacancies["items"])
+                print(f"Загружено вакансий: { len(page_vacancies['items']) }")
+            if len(page_vacancies["items"]) == 0:
                 break
-
+        return self.vacancies
 
 class SuperJobAPI(Engine):
     """Дочерний класс Engine для SuperJobAPI"""
 
     url = "https://api.superjob.ru/2.0/vacancies/"
 
-    def init(self, keyword):
+    def __init__(self, keyword):
         self.params = {
             "count": 100,
             "page": None,
@@ -105,7 +105,7 @@ class SuperJobAPI(Engine):
             "archive": False,
         }
         self.headers = {
-            "X-Api-App-Id": "v3.r.137756487.adf52d67576949fc227e9a44484f486d9ae8183c.c85c473c1a3d7451e8549432293d922bdcf27e12"
+            "X-Api-App-Id": value
         }
         self.vacancies = []
 
@@ -117,7 +117,7 @@ class SuperJobAPI(Engine):
 
     def get_formatted_vacancies(self):
         formatted_vacancies = []
-        #currencies = get_currencies()
+        currencies = get_currencies()
         sj_currencies = {
             "rub": "RUR",
             "uah": "UAH",
@@ -153,7 +153,7 @@ class SuperJobAPI(Engine):
         for page in range(pages_count):
             page_vacancies = []
             self.params["page"] = page
-            print(f"({self.__class__.name__}) Парсинг страницы { page } -", end=" ")
+            print(f"({self.__class__.__name__}) Парсинг страницы { page } -", end=" ")
             try:
                 page_vacancies = self.get_request()
             except ParsingError as error:
@@ -163,6 +163,7 @@ class SuperJobAPI(Engine):
                 print(f"Загружено вакансий: { len(page_vacancies) }")
             if len(page_vacancies) == 0:
                 break
+        return self.vacancies
 
 class Vacancy:
 
@@ -196,15 +197,18 @@ class Vacancy:
 Зарплата: { salary }
 Ссылка: { self.url }
         """
-    def __le__(self, other):
-        pass
-
     def __ge__(self, other):
-        pass
+        self.vacanсies = []
+        for self.salary_from in self.formatted_vacancies:
+            if self.salary_from >= other.salary_from:
+                self.vacanсies.extend(self.salary_from)
+        return self.vacanсies
 
+    # def __le__(self, other):
+    #     return self.salary_from >= other.salary_from
 class Connector:
 
-    def __init__(self, keyword, vacancies_json):
+    def __init__(self, keyword):
         self.filename = f"{ keyword.title() }.json"
         #self.insert(vacancies_json)
 
--- main.py
+++ main.py
@@ -1,28 +1,12 @@
 import requests
 from classes import HeadHunterAPI, SuperJobAPI, Vacancy, Connector
-
+from utils import get_currencies
 
 # Сохранение информации о вакансиях в файл
 # json_saver = JSONSaver()
 # json_saver.add_vacancy(vacancy)
 # json_saver.get_vacancies_by_salary("100 000-150 000 руб.")
 # json_saver.delete_vacancy(vacancy)
-#
-# Функция для взаимодействия с пользователем
-# def user_interaction():
-#     platforms = ["HeadHunter", "SuperJob"]
-#     search_query = input("Введите поисковый запрос: ")
-#     top_n = int(input("Введите количество вакансий для вывода в топ N: "))
-#     filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split()
-#     filtered_vacancies = filter_vacancies(hh_vacancies, superjob_vacancies, filter_words)
-#
-#     if not filtered_vacancies:
-#         print("Нет вакансий, соответствующих заданным критериям.")
-#         return
-#
-#     sorted_vacancies = sort_vacancies(filtered_vacancies)
-#     top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
-#     print_vacancies(top_vacancies)
 
 
 def main():
@@ -31,7 +15,7 @@ def main():
 
     """Создание экземпляров класса для работы с API сайтов с вакансиями"""
     hh = HeadHunterAPI(keyword)
-    sj = SuperJobAPI()
+    sj = SuperJobAPI(keyword)
 
     """Получение вакансий с разных платформ"""
     hh_vacancies = hh.get_vacancies()
@@ -40,7 +24,7 @@ def main():
     print(sj_vacancies)
 
     for api in (hh, sj):
-        api.get_vacancies(pages_count=10)
+        api.get_vacancies(pages_count=2)
         vacancies_json.extend(api.get_formatted_vacancies())
 
     connector = Connector(keyword=keyword, vacancies_json=vacancies_json)
@@ -63,10 +47,26 @@ def main():
         elif command == "1":
             vacancies = connector.select()
         elif command == "2":
-            vacancies = connector.sort_by_salary_from()
+            vacancies = connector.Vacancy
 
         for vacancy in vacancies:
             print(vacancy, end="\n")
+
+#Функция для взаимодействия с пользователем
+def user_interaction():
+    platforms = ["HeadHunter", "SuperJob"]
+    search_query = input("Введите поисковый запрос: ")
+    top_n = int(input("Введите количество вакансий для вывода в топ N: "))
+    filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split()
+    filtered_vacancies = Vacancy(filter_words)
+
+    if not filtered_vacancies:
+        print("Нет вакансий, соответствующих заданным критериям.")
+        return
+
+    sorted_vacancies = Vacancy(filtered_vacancies)
+    top_vacancies = Vacancy(sorted_vacancies, top_n)
+    print(top_vacancies)
 
 
 if __name__ == "__main__":
